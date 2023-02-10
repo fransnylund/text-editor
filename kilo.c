8b59e625f0155b3884d787994c5d9abfdc947593
Frans Nylund <frans1.nylund@gmail.com>
2023-02-10 11:10:30 +0200

    Clean up main(), print tildes on every line, clear screen, etc

--- kilo.c
+++ kilo.c
@@ -3,38 +3,54 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
 #include <errno.h>
 
+// defines
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 // data
 
-struct termios orig_termios;
+struct editorConfig {
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 // terminal
 
 void die(const char *s) {
+
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	// perror() looks at the errno variable and prints the error
 	perror(s);
 	exit(1);
 }
 
 void disable_raw_mode(void) {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
 		die("tcsetattr"); }
 }
 
 void enable_raw_mode(void) {
 	
 	// Get terminal attributes and store them in orig_termios
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
 		die("tcgetattr");
 	}
 
 	// When exiting, call disable_raw_mode()
 	atexit(disable_raw_mode);
 	
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 
 	// Disable Ctrl-s and Ctrl-q and some other attributes
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -58,29 +74,162 @@ void enable_raw_mode(void) {
 
 }
 
+// read one key and return it
+char editor_read_key() {
+
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return c;
+}
+
+int get_cursor_position(int *rows, int *cols) {
+
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return -1;
+
+	while (i < sizeof(buf) - 1) {
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+
+	buf[i] = '\0';
+	
+	// make sure it's an escape sequence
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return -1;
+
+	// parse the rows and cols values
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return -1;
+
+	return 0;
+
+}
+
+// get size of the terminal
+int get_window_size(int *rows, int *cols) {
+
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+			return -1;
+		return get_cursor_position(rows, cols);
+	}
+	else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+
+}
+
+// append buffer
+
+struct abuf {
+
+	char *buffer;
+	int len;
+
+};
+
+// this will be our struct initializer
+#define ABUF_INIT {NULL, 0}
+
+void ab_append(struct abuf *ab, const char *s, int len) {
+
+	char *new = realloc(ab->buffer, ab->len + len);
+
+	if (new == NULL)
+		return;
+
+	memcpy(&new[ab->len], s, len);
+	ab->buffer = new;
+	ab->len += len;
+}
+
+void ab_free(struct abuf *ab) {
+
+	free(ab->buffer);
+}
+
+// output
+
+// prints "~" on every line except last line
+void editor_draw_rows(struct abuf *ab) {
+
+	int y;
+	for (y = 0; y < E.screenrows; y++) {
+
+		ab_append(ab, "~", 1);
+
+		if (y < E.screenrows - 1)
+			ab_append(ab, "\r\n", 2);
+	}
+
+}
+
+// refreshes the screen by writing escape sequences
+void editor_refresh_screen() {
+
+	struct abuf ab = ABUF_INIT;
+
+	ab_append(&ab, "\x1b[2J", 4);
+	ab_append(&ab, "\x1b[H", 3);
+
+	editor_draw_rows(&ab);
+
+	ab_append(&ab, "\x1b[H", 3);
+
+	write(STDOUT_FILENO, ab.buffer, ab.len);
+	ab_free(&ab);
+}
+
+
+// input
+
+// handles a keypress from editorReadKey()
+void editor_process_keypress() {
+
+	char c = editor_read_key();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+			break;
+	}
+
+}
+
 // init
+
+// init_ediotr initializes all the stuff in the E struct
+void init_editor() {
+
+	if (get_window_size(&E.screenrows, &E.screencols) == -1)
+		die("get_window_size");
+}
 
 int main(void) {
 
 	enable_raw_mode();
+	init_editor();
 
 	while (1) {
-
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-			die("read");
-		}
-
-		// iscntrl() checks if a character is a control character
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		}
-		else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-
-		if (c == 'q')
-			break;
+		editor_refresh_screen();
+		editor_process_keypress();
 	}
 
 	return 0;
